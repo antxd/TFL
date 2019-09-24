230247a87894ad612b926573d837063d4480a494
Jose Marin <jdantrax@gmail.com>
2019-09-24 15:50:30 -0300

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 define('SITE', 'https://tfl.jdmm.xyz/');
 define('ABSPATH', dirname(__FILE__) . '/');
 define('DBHOST','localhost');
-define('DBUSER','josemcom_tfl');
-define('DBPASS','GbD(sGt.A_zo');
-define('DBNAME','josemcom_tfl');
+define('DBUSER','root');
+define('DBPASS','root');
+define('DBNAME','tfl');
 $paises = array("Argentina","Chile","Colombia","España","Perú","Reino Unido");
 $versiones = array(1 => "Prime", 2 => "Prime 2", 3 => "XX");
