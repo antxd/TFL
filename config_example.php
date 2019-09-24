1160fd31fd6de7f5b0247e4325c10788a61504e7
Jose Marin <jdantrax@gmail.com>
2019-09-24 16:09:06 -0300

    minor fix to run

---
+++ config_example.php
@@ -0,0 +1,10 @@
+<?php
+define('SITE', 'https://localhost/tfl/');
+define('ABSPATH', dirname(__FILE__) . '/');
+define('DBHOST','localhost');
+define('DBUSER','root');
+define('DBPASS','root');
+define('DBNAME','dbname');
+define('IMGBBAPIKEY','APIKEY');
+$paises = array("Argentina","Chile","Colombia","España","Perú","Reino Unido");
+$versiones = array(1 => "Prime", 2 => "Prime 2", 3 => "XX"); 
